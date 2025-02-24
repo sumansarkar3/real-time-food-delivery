6ed898915093d9607f2f5395eec9620b3a379978
suman <sumansarkar.dgp3@gmail.com>
2025-02-24 18:21:11 +0530

    All required files are added

---
+++ src/test/java/com/project/real_time_food_delivery/RealTimeFoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.real_time_food_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeFoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
